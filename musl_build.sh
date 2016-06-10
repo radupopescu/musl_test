2c4ebaef00d63999ed1a6248b0fd6e0b21c028ae
Radu Popescu <mail@radupopescu.net>
2016-06-11 00:33:59 +0200

    Setting WORKDIR in musl_build.sh

--- musl_build.sh
+++ musl_build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export WORKDIR=/opt/src
 rm -rf build-musl
 
-docker run --rm -it -v "${PWD}":/opt/src radupopescu/musl-builder sh -c "mkdir build-musl && cd build-musl && cmake -D CMAKE_EXE_LINKER_FLAGS="-static" ../ && make"
+docker run --rm -it -v "${PWD}":"${WORKDIR}" radupopescu/musl-builder \
+    sh -c "cd ${WORKDIR} && mkdir build-musl && cd build-musl && cmake -D CMAKE_EXE_LINKER_FLAGS=\"-static\" ../ && make"
